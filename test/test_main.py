cc3d50e2177fbf461268618b65aab88520976352
dunxuan <dunxuan@dunxuan.xyz>
2024-06-07 22:48:48 +0800

    modified:   src/main.py 	modified:   test/test_main.py

--- test/test_main.py
+++ test/test_main.py
@@ -1,4 +1,3 @@
-from pytest import MonkeyPatch  # noqa: F401
 from unittest.mock import patch
 import numpy as np
 
@@ -38,25 +37,32 @@ def test_is_slime_chunk():
 
 
 def test_get_mode(monkeypatch):
-    from src.main import get_mode
+    from src.main import get_mode, DEFAULT_MODE
 
     monkeypatch.setattr("builtins.input", lambda _: "10000")
     assert get_mode() == 10000
     monkeypatch.setattr("builtins.input", lambda _: "m")
-    assert get_mode() == 'm'
+    assert get_mode() == DEFAULT_MODE
+    monkeypatch.setattr("builtins.input", lambda _: "")
+    assert get_mode() == DEFAULT_MODE
+
 
 def test_get_radius(monkeypatch):
-    from src.main import get_radius
+    from src.main import get_radius, DEFAULT_RADIUS
 
     monkeypatch.setattr("builtins.input", lambda _: "10000")
     assert get_radius() == 10000
+    monkeypatch.setattr("builtins.input", lambda _: "")
+    assert get_radius() == DEFAULT_RADIUS
 
 
 def test_get_threshold(monkeypatch):
-    from src.main import get_threshold
+    from src.main import get_threshold, DEFAULT_THRESHOLD
 
     monkeypatch.setattr("builtins.input", lambda _: "100")
     assert get_threshold() == 100
+    monkeypatch.setattr("builtins.input", lambda _: "")
+    assert get_threshold() == DEFAULT_THRESHOLD
 
 
 def test_generate_seeds():
@@ -73,13 +79,13 @@ def test_generate_seeds():
 
 
 def test_detect_slime_chunk():
-    from src.main import detect_slime_chunk_parallel
+    from src.main import detect_slime_chunk
 
     real_detected_chunks = np.zeros((5, 5), dtype=bool)
     real_detected_chunks[0, 2] = True
     real_detected_chunks[4, 4] = True
 
-    _, detected_chunks = detect_slime_chunk_parallel(0, 2)
+    detected_chunks = detect_slime_chunk(0, 2)
 
     print("Detected Chunks:")
     print(detected_chunks)
@@ -91,10 +97,10 @@ def test_detect_slime_chunk():
 
 
 @patch("random.randint")
-def test_run_parallel(mock_randint, capsys):
-    from src.main import run_parallel
+def test_run(mock_randint, capsys):
+    from src.main import run
 
     mock_randint.return_value = 0
-    run_parallel(0, 0, 0)
+    run(0, 0, 0)
     captured = capsys.readouterr()
     assert captured.out == "史莱姆区块数: 12, 种子: 0, 挂机点区块位置: (0, 0)\n"
